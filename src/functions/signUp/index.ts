cb06ed0599667601515784bd70f0ecc8765ed5e0
Linus Ilbratt <linusilbratt@gmail.com>
2024-11-22 11:10:20 +0100

    implemented my error handling function in the signUp handler

--- src/functions/signUp/index.ts
+++ src/functions/signUp/index.ts
@@ -4,6 +4,8 @@ import bcrypt from 'bcryptjs';
 import { nanoid } from "nanoid";
 import { createAccount } from "./createAccount.js";
 import { checkUserName } from "./checkUserName.js";
+import { send } from "process";
+import { handleError } from "../../response/handleError.js";
 
 export const handler = async (
     event: APIGatewayProxyEvent
@@ -11,20 +13,29 @@ export const handler = async (
     try {
         const { username, password } = JSON.parse(event.body || "{}");
 
+        // Check if username already exists (400)
         const usernameExists = await checkUserName(username);
-
         if (usernameExists) {
-            return sendResponse(400, { sucess: false, message: "Username already exists"});
+            return handleError(400, "Username already exists");
         }
 
+        // Hash and salt the password
         const hashedPassword = await bcrypt.hash(password, 10);
-        const userId = nanoid();
+        const userId = nanoid(); // Generate a unique user-ID
 
+        // Creates an account (400 if it fails)
         const result = await createAccount(username, hashedPassword, userId);
-        return sendResponse(200, result)
+        if (!result.success) {
+            // Use specific error message if available, otherwise fallback to a generic message.
+            return handleError(400, result.message || "Could not create account"); 
+        }
+
+        // Registration success (200)
+        return sendResponse(200, { success: true, userId, message: "User created successfully" });
 
     } catch (error) {
-        console.error("Something went wrong:", error);
-        return sendResponse(400, { success: false, message: "Unable to process the request" });
+        console.error("Error in sign-up handler:", error);
+        // Unexpected error (500)
+        return handleError(500);
     }
 }
